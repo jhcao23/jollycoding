46a1184134b67d60518422f4a8c07448ab204ff9
John <jhcao23@gmail.com>
2023-03-16 19:29:58 -0700

    add cname

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://jhcao23.github.io',
-  baseUrl: '/my-docusaurus-project/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
